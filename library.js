fede703c0e5a8f130884cc9de66c42e5fa1506d6
QSL <kbeechly@icloud.com>
2022-08-18 23:00:20 -0700

    Add initial library display functionality

--- library.js
+++ library.js
@@ -17,4 +17,23 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function showLibrary() {
+    for (book in myLibrary) {
+        createCard(book);
+    }
+}
+
+function createCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const title = document.createElement('h2');
+    title.innerHTML = 'Title: ' + myLibrary[book].title;
+    card.appendChild(title);
+    cardCatalogue.appendChild(card);
+}
+
 const holes = new Book('Holes', 'Louis Sacher', '257', 'read');
+addBookToLibrary(holes);
+
+const autobioDH = new Book('Autobiography', 'Douglas Hofstadter', '1', 'read');
+addBookToLibrary(autobioDH);
